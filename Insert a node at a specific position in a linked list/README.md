e7354630fc7c91a1e9e3d6be91883fcbfed88e46
César Arturo González <a01261064@itesm.mx>
2017-09-05 23:52:33 -0500

    Insert a node at a specific position in a linked list

---
+++ Insert a node at a specific position in a linked list/README.md
@@ -0,0 +1,9 @@
+# Insert a node at a specific position in a linked list
+César Arturo González Pérez
+
+09/05/2017
+
+[Hackerrank](https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list)
+
+## Problem
+COMPLETE PROBLEM DOCUMENTATION
---
+++ Insert a node at a specific position in a linked list/Solution.java
@@ -0,0 +1,42 @@
+/**
+ * @author: César Arturo González Pérez
+ * @github: arturogonzalezp
+ * @webpage: http://arturogp.com
+ * 
+ * Hackerrank: https://www.hackerrank.com/challenges/crush
+ */
+
+/*
+      Insert Node at a given position in a linked list
+      head can be NULL
+      First element in the linked list is at position 0
+      Node is defined as
+      class Node {
+         int Data;
+         Node Next;
+      }
+    */
+
+    // This is a "method-only" submission.
+    // You only need to complete this method.
+
+    public static Node InsertNth(Node head, int data, int position)
+    {
+        Node pointNode = head;
+        Node tempNode = new Node();
+        tempNode.Data = data;
+        
+        if(position == 0){
+            tempNode.Next = head;
+            head = tempNode;
+        }else{
+            for(int i = 1; i < position; i++){
+                if(pointNode.Next == null)
+                    break;
+                pointNode = pointNode.Next;
+            }
+            tempNode.Next = pointNode.Next;
+            pointNode.Next = tempNode;
+        }
+        return head;
+    }
