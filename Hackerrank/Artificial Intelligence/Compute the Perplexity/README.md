3644818327477346050dbbab44775dbc2d11f1c8
César Arturo González <a01261064@itesm.mx>
2017-09-07 18:53:52 -0500

    Compute the Perplexity

---
+++ Hackerrank/Artificial Intelligence/Compute the Perplexity/README.md
@@ -0,0 +1,9 @@
+# Compute the Perplexity
+César Arturo González Pérez
+
+09/07/2017
+
+[Hackerrank](https://www.hackerrank.com/challenges/nlp-compute-the-perplexity/)
+
+## Problem
+COMPLETE PROBLEM DOCUMENTATION
---
+++ Hackerrank/Artificial Intelligence/Compute the Perplexity/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * @author: César Arturo González Pérez
+ * @github: arturogonzalezp
+ * @webpage: http://arturogp.com
+ * 
+ * Hackerrank: https://www.hackerrank.com/challenges/nlp-compute-the-perplexity/
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        double cross_entropy = 9.91;
+        int result = (int) Math.pow(2,cross_entropy);
+        System.out.println(result);
+        
+    }
+}
