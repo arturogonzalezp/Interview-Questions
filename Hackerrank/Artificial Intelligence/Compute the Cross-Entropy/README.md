99662f456f5f2edaf334fbc851e0d052975ef8cb
César Arturo González <a01261064@itesm.mx>
2017-09-07 19:07:00 -0500

    Compute the Cross-Entropy

---
+++ Hackerrank/Artificial Intelligence/Compute the Cross-Entropy/README.md
@@ -0,0 +1,9 @@
+# Compute the Cross-Entropy
+César Arturo González Pérez
+
+09/07/2017
+
+[Hackerrank](https://www.hackerrank.com/challenges/nlp-compute-the-cross-entropy/)
+
+## Problem
+COMPLETE PROBLEM DOCUMENTATION
---
+++ Hackerrank/Artificial Intelligence/Compute the Cross-Entropy/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * @author: César Arturo González Pérez
+ * @github: arturogonzalezp
+ * @webpage: http://arturogp.com
+ * 
+ * Hackerrank: https://www.hackerrank.com/challenges/nlp-compute-the-cross-entropy/
+ */
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        double cross_entropy = Math.log(170) / Math.log(2);
+        System.out.println(cross_entropy);
+    }
+}
